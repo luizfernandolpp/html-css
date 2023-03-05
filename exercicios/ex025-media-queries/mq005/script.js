2beb94d8b2f04bbd293c11f4cf2e4d1f0a5c836e
Luiz Fernando <111098745+luizfernandolpp@users.noreply.github.com>
2023-03-05 00:23:25 -0300

    Update script.js

--- exercicios/ex025-media-queries/mq005/script.js
+++ exercicios/ex025-media-queries/mq005/script.js
@@ -1,9 +1,11 @@
 function clickMenu(){
     let menu = window.document.querySelector('menu');
+    let body = window.document.querySelector('body');
     if (menu.style.display == 'block'){
-        menu.style.display = 'none'
+        menu.style.display = 'none';
     } else{
-        menu.style.display = 'block'
+        menu.style.display = 'block';
+        
     }
 }
 
